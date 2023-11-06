e407766c108c2d1af910bdc9577ab10e68e27fc6
Khitrov <Hitrov.95@yandex.ru>
2023-11-06 15:05:43 +0300

    added tests, functions, code, readme

--- codes/code.py
+++ codes/code.py
@@ -1,1 +1,20 @@
-import json
+# Импортируем функции:
+from functions import (json_encode, operation_date, last_five_operations, sorted_last_five, replace_number)
+
+# Код для операций:
+list_main = sorted_last_five(
+    last_five_operations(json_encode("operations.json"), operation_date(json_encode("operations.json"))))
+for every in list_main:
+    try:
+        date = every["date"].strftime("%d.%m.%Y")
+        desc = every["description"]
+        from_who = replace_number(every.get("from", ""))
+        to_who = replace_number(every["to"])
+        sum_oper = every["operationAmount"]["amount"]
+        currency = every["operationAmount"]["currency"]["name"]
+        print(f"""{date} {desc}
+{from_who} -> {to_who}
+{sum_oper} {currency}
+""")
+    except KeyError:
+        continue
---
+++ codes/functions.py
@@ -0,0 +1,62 @@
+# Импортируем модули:
+import json
+import datetime
+
+
+# Функции:
+def json_encode(name):
+    """Берет данные из .json"""
+    with open(name, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def operation_date(operation_list):
+    """Возвращает дату и время последних пяти операций"""
+    date_of_operation = []
+    for operations in operation_list:
+        try:
+            if operations["state"] == "EXECUTED":
+                date_of_operation.append(datetime.datetime.fromisoformat(operations["date"]))
+        except KeyError:
+            continue
+    return sorted(date_of_operation)[-5:]
+
+
+def last_five_operations(list_operations, last_five_datetime):
+    """Возвращает данные последних пяти операций"""
+    last_five = []
+    for operation in list_operations:
+        try:
+            if datetime.datetime.fromisoformat(operation["date"]) in last_five_datetime:
+                last_five.append(operation)
+        except KeyError:
+            continue
+    return last_five
+
+
+def sorted_last_five(last_five_operations):
+    """Сортирует последние пять операций по порядку от последней"""
+    last_five = last_five_operations
+    for every in last_five:
+        every["date"] = datetime.datetime.fromisoformat(every["date"])
+    sorted_last_five = sorted(last_five, key=lambda x: x["date"], reverse=True)
+    return sorted_last_five
+
+
+def replace_number(string_number):
+    """Шифрует нужные цифры"""
+    text_list = string_number.split()
+    try:
+        replace_number = ", ".join(([text_list[-1][length:length + 4] for length in range(0, len(text_list[-1]), 4)]))
+    except IndexError:
+        return ""
+    replace_number_min = replace_number.replace(",", "")
+    if text_list[0] != "Счет":
+        replace_number_finally = replace_number_min[:7] + "** ****" + replace_number_min[-5:]
+    else:
+        replace_number_finally = "**" + replace_number_min[-4:]
+    text_list_replace = text_list[:-1]
+    text_list_replace.append(replace_number_finally)
+    text_replace = ", ".join(text_list_replace).replace(",", "")
+    return text_replace
